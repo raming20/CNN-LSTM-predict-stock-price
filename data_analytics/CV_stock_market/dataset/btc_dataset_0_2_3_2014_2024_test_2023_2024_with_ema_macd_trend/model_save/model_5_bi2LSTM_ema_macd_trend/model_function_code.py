5d48945f0fc5364bca1e1b3f2a0b7353fe56b844
Hieucaohd <hieucaohd@gmail.com>
2024-11-23 10:38:13 +0700

    ok

---
+++ data_analytics/CV_stock_market/dataset/btc_dataset_0_2_3_2014_2024_test_2023_2024_with_ema_macd_trend/model_save/model_5_bi2LSTM_ema_macd_trend/model_function_code.py
@@ -0,0 +1,35 @@
+import keras
+
+def model_5_biLSTM_with_ema_macd_trend(image_shape, days_result, days_of_ema_9=2, days_of_macd_history=2):
+    trend_type_input = keras.layers.Input(shape=(1,), name="trend_type_input")
+    x1 = keras.layers.Dense(8, activation='relu')(trend_type_input)
+    
+    image_input = keras.layers.Input(shape=image_shape, name="image_input")
+    x2 = keras.layers.Conv2D(8, (2, 2), activation='relu')(image_input)
+    x2 = keras.layers.MaxPooling2D((2, 2), strides=(2, 2))(x2)
+    x2 = keras.layers.Flatten()(x2)
+    x2 = keras.layers.Dense(64, activation='relu')(x2)
+    x2 = keras.layers.Dropout(0.1)(x2)
+    
+    ema_9_input = keras.layers.Input(shape=(days_of_ema_9,), name="ema_9_input")
+    x3 = keras.layers.Dense(8, activation='relu')(ema_9_input)
+    
+    macd_history_input = keras.layers.Input(shape=(days_of_macd_history,), name="macd_history_input")
+    x4 = keras.layers.Dense(8, activation='relu')(macd_history_input)
+    
+    combined = keras.layers.concatenate([x3, x4, x1, x2])
+    
+    z = keras.layers.Dense(64, activation='relu')(combined)
+    z = keras.layers.Dropout(0.1)(z)
+    z = keras.layers.Dense(64, activation='relu')(z)
+    
+    input_lstm = keras.layers.RepeatVector(days_result)(z)
+    lstm = keras.layers.Bidirectional(keras.layers.LSTM(128, activation="tanh", return_sequences=True))(input_lstm)
+    lstm = keras.layers.Dropout(0.1)(lstm)
+    lstm = keras.layers.Bidirectional(keras.layers.LSTM(128, activation="tanh", return_sequences=True))(lstm)
+    lstm = keras.layers.Dropout(0.1)(lstm)
+    output_lstm = keras.layers.TimeDistributed(keras.layers.Dense(2))(lstm)
+    
+    model = keras.models.Model(inputs=[ema_9_input, macd_history_input, trend_type_input, image_input], outputs=output_lstm)
+    
+    return model, "model_5_biLSTM_ema_macd_trend"
